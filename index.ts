98f65c8609840f3da298c51c2cd670e5f028e031
saeed <saeed.da1994@gmail.com>
2023-01-08 22:14:20 +0330

    use interface to describe an object and use in as a type

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
+interface PostGateKeeper {
+    title: string;
+    ageDay: number;
+    publish: boolean;
+}
+
 const post= {
     title: 'this is a new post',
     ageDay: 10,
     publish: true
 }
 
-const show = (addPostHere : {title: string, ageDay: number, publish: boolean})=>{
+const show = (addPostHere : PostGateKeeper)=>{
     console.log(addPostHere)
     return `the title of post is : (${post.title}) and published ${post.ageDay} days ago`
 }
