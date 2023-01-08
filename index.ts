d80b2aff5fd5807ddb3a2f303a367c220fa9d019
saeed <saeed.da1994@gmail.com>
2023-01-08 17:15:54 +0330

    test func type

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 console.log('hello ts')
-const nothing: null= null;
-const undef: undefined= undefined;
+const nothing: null = null;
+const undef: undefined = undefined;
+const sentence: string = 'this is a test for ts';
+console.log(sentence.includes('is'))
+const f = (a: number, b: number): number => {
+    return a + b;
+}
