3525d1d8c0dac5643178396681dc3cdd1f4c1c37
saeed <saeed.da1994@gmail.com>
2023-01-09 19:08:46 +0330

    use interface in class

--- index.ts
+++ index.ts
@@ -1,4 +1,24 @@
-import {Car} from "./Car";
+interface Drivable {
+    year: number;
+    color: string;
 
-const car = new Car('red', 1994);
-car.drive();
+    drive(): string;
+
+}
+
+class Car implements Drivable {
+    year= 1994;
+    color= 'red';
+
+
+    drives(): string {
+        return `hello this car made at ${this.year} and color is ${this.color}`
+    }
+
+    drive(): string {
+        return "";
+    }
+}
+
+const car = new Car();
+console.log(car.drives())
